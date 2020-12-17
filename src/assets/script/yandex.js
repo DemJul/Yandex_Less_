24c8611f2bc860d1e104900cfdd4161f1fd7b9b6
Unknown <dementieva2@gmail.com>
2020-12-17 15:13:26 +0300

    scroll

--- src/assets/script/yandex.js
+++ src/assets/script/yandex.js
@@ -1,61 +1,54 @@
 window.addEventListener("scroll", function(){
            
-    let block = document.querySelector('.block');
-    //высота блока контента вместе с границами
-    let height = block.offsetHeight;  
-    // текущее положение скролбара    
-    let scrollY = window.pageYOffset;     
-    // высота внутренней области окна документа
-    let window_height = window.innerHeight;      
-    let coordY = scrollY + window_height;
-   
-    if(coordY >= height)
-    {
-        for (let i = 0; i<=3;i++){
-            block.appendChild(renderBlock());
-        }
-    }
+  let block = document.querySelector('.block');
+ 
+  if((document.documentElement.scrollHeight - document.documentElement.scrollTop - document.documentElement.clientHeight) < 1)
+  {
+      for (let i = 0; i<=3;i++){
+          block.appendChild(renderBlock());
+      }
+  }
 });
 
 const renderBlock= () => {
-    const item = document.createElement("div");
-    item.classList.add("block__item");
-    return item;
+  const item = document.createElement("div");
+  item.classList.add("block__item");
+  return item;
 }
 
 let writeSize = () => {
-    console.log(`Ширина: ${window.innerWidth}, Высота: ${window.innerHeight}`);
+  console.log(`Ширина: ${window.innerWidth}, Высота: ${window.innerHeight}`);
 };
 
 window.addEventListener("resize", throttle(writeSize,2000));
 
 function throttle(func, ms) {
 
-  let isThrottled = false,
-    savedArgs,
-    savedThis;
-    console.log(isThrottled);
+let isThrottled = false,
+  savedArgs,
+  savedThis;
+  console.log(isThrottled);
 
-  function wrapper() {
+function wrapper() {
 
-    if (isThrottled) { 
-      savedArgs = arguments;
-      savedThis = this;
-      return;
-    }
-
-    func.apply(this, arguments);
-
-    isThrottled = true;
-
-    setTimeout(function() {
-      isThrottled = false; 
-      if (savedArgs) {
-        wrapper.apply(savedThis, savedArgs);
-        savedArgs = savedThis = null;
-      }
-    }, ms);
+  if (isThrottled) { 
+    savedArgs = arguments;
+    savedThis = this;
+    return;
   }
 
-  return wrapper;
+  func.apply(this, arguments);
+
+  isThrottled = true;
+
+  setTimeout(function() {
+    isThrottled = false; 
+    if (savedArgs) {
+      wrapper.apply(savedThis, savedArgs);
+      savedArgs = savedThis = null;
+    }
+  }, ms);
+}
+
+return wrapper;
 }
