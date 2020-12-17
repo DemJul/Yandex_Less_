195d9c9095fad03447ce49b6efd16d794e360721
Unknown <dementieva2@gmail.com>
2020-12-17 03:12:41 +0300

    edit js

--- src/assets/js/c-script.js
+++ src/assets/js/c-script.js
@@ -1,6 +1,7 @@
 const searchMiddle = document.querySelector('.c-header-middle__search-is-open');
 const searchMiddleActive = document.querySelector('.c-header-middle__search-is-close');
 const searchInputActive = document.querySelector('.c-search');
+const search = document.querySelector('.c-header-middle__search');
 
 const focusInputActive = document.querySelector('.c-search__input');
 const menu = document.querySelector('.c-header-middle__menu');
@@ -8,8 +9,7 @@ const menuActive = document.querySelector('.c-header-middle__menu-is-close');
 const menuBurger = document.querySelector('.c-header-middle__menu-is-open');
 const menuOtherMb = document.querySelector('.c-menu-mobile');
 
-window.addEventListener('click', event => {
-    if (event.target.classList.contains('c-header-middle__search')) {
+search.addEventListener('click', event => {
         searchMiddle.classList.toggle('_hidden');
         searchMiddle.classList.toggle('_visible');
         searchMiddleActive.classList.toggle('_hidden');
@@ -18,15 +18,14 @@ window.addEventListener('click', event => {
         if (searchMiddleActive.classList.contains('_visible')){
             focusInputActive.focus();
         }
-        console.log(event.target);
-    } else if (event.target.classList.contains('c-header-middle__menu')) {
-        menuActive.classList.toggle('_hidden');
-        menuActive.classList.toggle('_visible');
-        menuBurger.classList.toggle('_hidden');
-        menuBurger.classList.toggle('_visible');
-        menuOtherMb.classList.toggle('_visible');
-    }
-})
+    });
+menu.addEventListener('click', event => {
+    menuActive.classList.toggle('_hidden');
+    menuActive.classList.toggle('_visible');
+    menuBurger.classList.toggle('_hidden');
+    menuBurger.classList.toggle('_visible');
+    menuOtherMb.classList.toggle('_visible');
+});
 
 window.addEventListener('resize', () => {
     if(window.innerWidth >= 1250) {
